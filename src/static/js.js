8089a9315e135ff7f80b49ef41532f0925e16135
Jean-Christophe Grondin <groj2904@dinf-0014-13b.DInf.fsci.usherbrooke.ca>
2015-02-13 11:25:43 -0500

    Adding structured client repr. work in progress

--- src/static/js.js
+++ src/static/js.js
@@ -4,91 +4,199 @@ LINE_REPR_TAG = '<' + LINE_REPR +'>';
 LINE_CLASS_VISIBLE = 'line';
 LINE_CLASS_INVISIBLE = 'emptyLine';
 
-// Create see_table
-// It index represent a line and each value is the size of the line
-var seek_table = [];
-seek_table[0] = 0;
+// Central point of interractions
+DEFAULT_PUSH_INTERVAL = 2000; // ms
+communicator = null;
 
-// Represent the numper of lines added
-var max_number_of_lines_reached = 0;
 
 // Initialize content when ready
 $(document).ready(init);
-
-
 function init() {
-  // Execute request to get first file
-  var fileContent = "Hello\nWorld";
-  updateTextArea(fileContent, 0);
-  updateDisplay();
+  // Classes
+  var zoneLocal = new LocalZone('editorLocal');
+  var zoneLastVersion = new LastVersionZone('editorLastVersion');
+  var zoneDisplay = new DisplayZone('editorDisplay');
+
+  communicator = new Communicator(zoneLocal, zoneLastVersion, zoneDisplay);
+  communicator.showFileContent("dummyStringForNow");
 }
-
-
-function updateSeekTable(){
-  seek_table = $('#editorInput').val().split("\n").map(function(line){
-    return line.length;
-  });
-}
-
-
-function updateTextArea(newVal, at){
-  var editor_zone = $('#editorInput');
-  var current_pos = editor_zone.getCursorPosition();
-  
-  // Add new text
-  var content = editor_zone.val();
-  editor_zone.val(content.slice(0, at) + newVal + content.slice(at));
-  // Restore cursor position and
-  // adjust cursor position if added text was before
-  current_pos += (at < current_pos) ? newVal.length : 0;
-  editor_zone.setCursorPosition(current_pos);
-}
-
-
-function updateDisplay(){
-  var lines = htmlEncode($('#editorInput').val()).split("\n");
-  var linesContainer = $("#editorDisplay");
-  
-  // Repopulate existing line-tags
-  var index;
-  var max_loop = Math.min(lines.length, max_number_of_lines_reached);
-  for(index = 0; index < max_loop; ++index){
-    var lineNode = linesContainer.children().eq(index);
-    lineNode.text(lines[index]);
-    lineNode.attr('class', LINE_CLASS_VISIBLE); // Required when line was invisible
-  }
-
-  // Add new line-tags
-  for( ; index < lines.length; ++index){
-    linesContainer.append(
-      // Create line tag
-      $(LINE_REPR_TAG).attr('class', LINE_CLASS_VISIBLE)
-                      .append(lines[index]));
-    ++max_number_of_lines_reached;
-  }
-
-  // Hide other lines
-  // Don't delete them 
-  for( ; index < max_number_of_lines_reached; ++index){
-    linesContainer.children().eq(index).attr('class', LINE_CLASS_INVISIBLE);
-  }
-}
-
 
 
 // TEST_ONLY
 function addNewTextAt(){
   var content = $('#addText').val();
   var at = $('#addAt').val();
-  updateTextArea(content, at);
+  registerUserUpdate(content, at);
+}
+
+function test_queue() {
+  registerServerUpdate("X", 0);
+  registerServerUpdate("Y", 11);
+  registerServerUpdate("Z", 3);
 }
 
 
+setTimeout(function t() {
+  communicator.notifySoft("abc", 0);
+}, 2000);
 
-function test() {
-  updateDisplay();
+// #####################################
+// #####                           #####
+// #####          Classes          #####
+// #####                           #####
+// #####################################
+
+// JSON Representation helpers
+function createModifGroup(deltas) { return {revision: 0, deltas: deltas}; }
+function createModif(val, at) { return {val: val, at: at}; }
+
+
+function Communicator(zl, zlv, zd, pushInterval) {
+
+  // Handle null value
+  pushInterval = pushInterval || DEFAULT_PUSH_INTERVAL;
+
+  this.zoneLocal = zl;
+  this.zoneLastVersion = zlv;
+  this.zoneDisplay = zd;
+  this.fileRevision = 0;
+
+  this.showFileContent = function(filepath) {
+    // Do request
+    var content = "Hello\nWorld";
+    // The revision from the response
+    this.fileRevision = 0;
+    this.notifyForce(content, 0);
+  };
+
+  this.send = function(modifications) {
+    // ajax
+  };
+
+  this.receive = function(modifications){
+    // websocket
+  };
+
+  this.notifyForce = function(newVal, at) {
+    this.zoneLastVersion.put(newVal, at);
+    this.zoneLocal.clear();
+    this.zoneDisplay.update(newVal);
+  };
+
+  this.notifySoft = function(newVal, at) {
+    // Quickhack
+    var repr = [createModif(newVal, at)];
+    this.zoneLastVersion.update(repr);
+    this.zoneLocal.update(newVal, at);
+    this.zoneDisplay.update(this.combineText());
+  };
+
+  this.combineText = function() {
+    //this.zoneLocal.get
+    //this.zoneLastVersion = zlv;
+    return "Combined magic";
+  };
+
+  this.pushChanges = function() {
+    // try push
+    var changes = this.zoneLocal.get();
+    if(changes.length == 0) return;
+    // Send
+    this.send(changes);
+  };
+  //this.pushIntervalHandle = setInterval(this.pushChanges, pushInterval);
+
+
+};
+
+function DisplayZone(zoneID){
+  this.node = $("#" + zoneID);
+  // Represent the numper of lines added
+  this.max_number_of_lines_reached = 0;
+
+  this.update = function(text){
+    var lines = text.split("\n");
+    // Repopulate existing line-tags
+    var index;
+    var max_loop = Math.min(lines.length, this.max_number_of_lines_reached);
+    for(index = 0; index < max_loop; ++index){
+      var lineNode = this.node.children().eq(index);
+      lineNode.text(lines[index]);
+      // Required when line was invisible
+      lineNode.attr('class', LINE_CLASS_VISIBLE); 
+    }
+    // Add new line-tags
+    for( ; index < lines.length; ++index){
+      this.node.append(
+        // Create line tag
+        $(LINE_REPR_TAG).attr('class', LINE_CLASS_VISIBLE)
+                        .append(lines[index]));
+      ++this.max_number_of_lines_reached;
+    }
+    // Hide other lines
+    // Don't delete them 
+    for( ; index < this.max_number_of_lines_reached; ++index){
+      this.node.children().eq(index).attr('class', LINE_CLASS_INVISIBLE);
+    }
+  };
 }
 
+function LastVersionZone(zoneID) {
+  this.zone = $("#" + zoneID);
+
+  this.add = function(newVal, at){
+    // Add new text
+    var content = this.zone.val();
+    this.zone.val(content.slice(0, at) + newVal + content.slice(at));
+  };
+
+  this.update = function(modifications) {
+    for(var i = 0; i < modifications.length; ++i) {
+      this.add(modifications[i].val, modifications[i].at);
+    }
+  };
+
+  this.put = function(text) {
+    this.zone.val(text);
+  };
+
+  this.get = function() {
+     return this.zone.val();
+  };
+}
+
+function LocalZone(zoneID) {
+  this.zone = $("#" + zoneID);
+
+  this.modifications = [];
+  this.currentModificationPos = 0;
+
+  this.get = function() {
+    //return this.modifications;
+    return createModif(this.zone.val(), this.currentModificationPos);
+  };
+
+  this.pos = function() {
+    return this.currentModificationPos;
+  };
+
+  this.clear = function() {
+    this.zone.val("");
+    this.modifications.clear();
+    this.currentModificationPos = 0;
+  };
+
+  this.update = function(val, at) {
+    this.currentModificationPos += (at < this.currentModificationPos) ? val.length : 0;
+  };
+}
+
+
+// ###################################
+// #####                         #####
+// #####         Helpers         #####
+// #####                         #####
+// ###################################
 
 /* SELECTION HELPER FUNCTIONS */
 $.fn.selectRange = function(start, end) {
@@ -113,11 +221,22 @@ $.fn.selectRange = function(start, end) {
 $.fn.setCursorPosition = function(pos) { this.selectRange(pos, pos); };
 $.fn.getCursorPosition = function() { return this.prop("selectionStart"); };
 
-/* HTML STATEMENT HELPER FUNCTIONS */
-function htmlEncode(value) { return $('<div/>').text(value).html(); }
-function htmlDecode(value) { return $('<div/>').html(value).text(); }
+/* ARRAY HELPER */
+Array.prototype.clear = function() {
+  while (this.length) {
+    this.pop();
+  }
+};
 
-
+/* MEASUREMENT HELPER */
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
 
 
 /*
