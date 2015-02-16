1ba511cae4aa7bb3912280b9ba322f9caee048c6
Vincent Philippon <Vincent.Philippon@USherbrooke.ca>
2015-02-16 15:42:50 -0500

    JSON communication draft

--- src/server/ideController.py
+++ src/server/ideController.py
@@ -1,4 +1,5 @@
 import cherrypy
+import simplejson
 from ws4py.websocket import WebSocket
 from genshi.template import TemplateLoader
 from threading import Lock
@@ -27,26 +28,55 @@ class IDEController(object):
 
   @cherrypy.expose
   def index(self):
-    return "Hello world %s" % self.data
+    """
+    IDEController index page generator
+    (Path : /ide/)
+    """
     # TODO Return page/template render for the IDE part
-    # tmpl = loader.load('edit.html')
-    # # set args in generate as key1=val1, key2=val2
-    # stream = tmpl.generate()
-    # return stream.render('html')
+    tmpl = self._loader.load('edit_test.html')
+    # set args in generate as key1=val1, key2=val2
+    stream = tmpl.generate()
+    return stream.render('html')
 
   @cherrypy.expose
-  def save(self, content):
+  @cherrypy.tools.json_out()
+  @cherrypy.tools.json_in()
+  def save(self):
+    """
+    Receive changes to a file from the client
+    (Path : /ide/save)
+
+    Input must be JSON of the following format:
+      TODO DESCRIBE JSON
+
+    Output on the WS will be JSON of the following format:
+      TODO DESCRIBE JSON
+    """
+    self._logger.info("Changes received from {0}:{1}".format(cherrypy.request.remote.ip,
+                                                             cherrypy.request.remote.port))
+    input_json = cherrypy.request.json
+
     # XXX Temp dummy content for test
-    self.data += content
-    sender_addr = (cherrypy.request.remote.ip, cherrypy.request.remote.port)
-    for user in IDEWebSocket.IDEClients:
-      if user.peer_address != sender_addr:  # XXX Must check ifremote ws port is the same as request
-        user.send(self.data)
+    self.data += input_json['content']
+    for client in IDEWebSocket.IDEClients:
+      client.send(simplejson.dumps({"content": self.data}))
 
   @cherrypy.expose
+  @cherrypy.tools.json_out()
+  @cherrypy.tools.json_in()
   def dump(self):
+    """
+    Sends the current content of a given file
+    (Path : /ide/dump)
+
+    Input must be JSON of the following format:
+      TODO DESCRIBE JSON
+
+    @return: JSON of the following format:
+      TODO DESCRIBE JSON
+    """
     # XXX Temp dummy content
-    return self.data
+    return {"content": self.data}
 
   @cherrypy.expose
   def ws(self):
@@ -58,6 +88,9 @@ class IDEController(object):
 
 
 class IDEWebSocket(WebSocket):
+  """
+  WebSocket for the IDEController
+  """
   IDEClients = set()
   __lock = Lock()
 
---
+++ src/static/edit_test.js
@@ -0,0 +1,72 @@
+
+$(document).ready(function() {
+  if(!("WebSocket" in window)){
+    alert("BOOM NO WEBSOCKET");
+  }else{
+    connect();
+  }
+});
+
+$('#editor').keypress(function(event) {
+  if (event.which == 13) {  //Enter Key
+    event.preventDefault();
+    sendChanges();
+  }
+});
+
+function connect() {
+  //var host = "ws://localhost:8080/ide/ws";
+  var host = "ws://10.44.88.142:8080/ide/ws";
+  var retryTimeout;
+  var socket = new WebSocket(host);
+
+  socket.onopen = function(){
+    clearTimeout(retryTimeout);
+    alert('WSOPEN');
+    refreshChat();
+  }
+
+  socket.onmessage = function(msg){
+    var json_result = jQuery.parseJSON(msg.data);
+    $('#content').html(json_result.content);
+  }
+
+  socket.onclose = function(){
+    clearTimeout(retryTimeout);
+    alert('WSCLOSE');
+    retryTimeout = setTimeout(function(){
+      connect();
+    }, 3000);
+  }      
+}
+
+function sendChanges() {
+  modification = {
+    "content": $('#editor').val() + '<br>'
+  };
+  $.ajax({
+    type: "POST",
+    url: "save",
+    contentType: "application/json",
+    dataType: "json",
+    data: JSON.stringify(modification),
+    cache: false,
+    success: function(data) {
+      console.log('SEND SUCCESS')
+      $('#editor').val('');
+    }
+  });
+}
+
+function refreshChat() {
+  $.ajax({
+    type: "GET",
+    url: "dump",
+    dataType: "text",
+    success: function(data) {
+      var json_result = jQuery.parseJSON(data);
+      console.log('REFRESH SUCCESS')
+      $('#content').html(json_result.content);
+    }
+  });
+}
