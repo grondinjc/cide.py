030560bb1e406be4930212fcd063076b10f6c9dc
Vincent Philippon <Vincent.Philippon@USherbrooke.ca>
2015-02-17 19:29:19 -0500

    Docstring + session usage for WS user link

--- src/server/ideController.py
+++ src/server/ideController.py
@@ -2,7 +2,6 @@ import cherrypy
 import simplejson
 from ws4py.websocket import WebSocket
 from genshi.template import TemplateLoader
-from threading import Lock
 
 
 class IDEController(object):
@@ -31,7 +30,10 @@ class IDEController(object):
     """
     IDEController index page generator
     (Path : /ide/)
+
+    @return: Template HTML render
     """
+    cherrypy.session['username'] = 'test'
     # TODO Return page/template render for the IDE part
     tmpl = self._loader.load('edit_test.html')
     # set args in generate as key1=val1, key2=val2
@@ -47,10 +49,25 @@ class IDEController(object):
     (Path : /ide/save)
 
     Input must be JSON of the following format:
-      TODO DESCRIBE JSON
+      {
+        'file':    '<<Filepath of edited file>>',
+        'vers':    '<<File version>>',
+        'type':    '<<Type of edit (ins | del)>>',
+        'pos':     '<<Position of edit>>',
+        'content': '<<Content of insert | Number of deletes>>'
+      }
 
     Output on the WS will be JSON of the following format:
-      TODO DESCRIBE JSON
+      {
+        'file':    '<<Filepath of edited file>>',
+        'vers':    '<<File version>>',
+        'type':    '<<Type of edit (ins | del)>>',
+        'pos':     '<<Position of edit>>',
+        'content': '<<Content of insert | Number of deletes>>'
+      }
+
+    @return: If ok: Nothing + (200)
+             If version is too old : Nothing + (410 - Gone)
     """
     self._logger.info("Changes received from {0}:{1}".format(cherrypy.request.remote.ip,
                                                              cherrypy.request.remote.port))
@@ -69,11 +86,22 @@ class IDEController(object):
     Sends the current content of a given file
     (Path : /ide/dump)
 
+    User must start to buffer changes received for file before requesting dump.
+    The server will register the user to receive updates, then send the dump,
+    so the user misses no update. The user will have to detect which updates
+    to apply based on the ``version`` sent.
+
     Input must be JSON of the following format:
-      TODO DESCRIBE JSON
+      {
+        'file': '<<Filepath of requested file>>'
+      }
 
     @return: JSON of the following format:
-      TODO DESCRIBE JSON
+      {
+        'file':    '<<Filepath of given file>>',
+        'vers':    '<<File version>>',
+        'content': '<<Content of the requested file>>'
+      }
     """
     # XXX Temp dummy content
     return {"content": self.data}
@@ -92,14 +120,16 @@ class IDEWebSocket(WebSocket):
   """
   WebSocket for the IDEController
   """
-  IDEClients = set()
-  __lock = Lock()
+  IDEClients = {}
 
   def __init__(self, *args, **kw):
     WebSocket.__init__(self, *args, **kw)
-    with self.__lock:
-      self.IDEClients.add(self)
+    self.username = None
+
+  def opened(self):
+    self.username = cherrypy.session['username']
+    self.IDEClients[self.username] = self
 
   def closed(self, code, reason=None):
-    self.IDEClients.remove(self)
+    del self.IDEClients[self.username]
 
