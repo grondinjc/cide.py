99fa007a461f28d5164d1a2ef75a868b03c5e1ab
grondinjc <jean-christophe.grondin@usherbrooke.ca>
2015-03-05 17:08:25 -0500

    Adding SASS support for css files

--- bin/startCIDE.py
+++ bin/startCIDE.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import sass
 import logging
 import cherrypy
 from configobj import ConfigObj
@@ -9,6 +10,8 @@ from cide.server.ideController import IDEController
 from cide.server.chatController import ChatController
 from cide.app.python.chat import Chat
 
+PREPROCESSOR_STYLE_EXT = '.scss'
+COMPILED_STYLE_EXT = '.css'
 
 # Read config file name from command parameters
 if len(sys.argv) != 2:
@@ -19,7 +22,9 @@ else:
 
 # Get the server and app config from the config file received
 bin_dir = os.path.abspath(os.path.dirname(__file__))
-templates_dir = os.path.join(bin_dir, '../src/templates')
+templates_dir = os.path.normpath(os.path.join(bin_dir, '../src/templates'))
+preprocessor_dir = os.path.normpath(os.path.join(bin_dir, '../src/preprocessor'))
+compiled_dir = os.path.normpath(os.path.join(bin_dir, '../src/static/css'))
 
 configs = ConfigObj(configs_file)
 
@@ -28,6 +33,21 @@ welcomeController_conf_file = configs['DEFAULT']['welcomeController']
 ideController_conf_file = configs['DEFAULT']['ideController']
 chatController_conf_file = configs['DEFAULT']['chatController']
 cide_log_file = configs['DEFAULT']['log_file']
+
+# Detect all SASS files
+scss_files = ["{0}/{1}".format(preprocessor_dir,filename) 
+              for filename in os.listdir(preprocessor_dir) 
+              if filename.endswith(PREPROCESSOR_STYLE_EXT)]
+
+# Process all detected SASS files
+for scss in scss_files:
+  compiled_css = sass.compile(filename=scss)
+  compiled_css_path = os.path.join(compiled_dir, 
+                                   os.path.basename(scss).replace(PREPROCESSOR_STYLE_EXT, 
+                                                                  COMPILED_STYLE_EXT))
+  # Open file in write mode to override any existing content
+  with open(compiled_css_path, 'w') as compiled_file:
+    compiled_file.write(compiled_css)
 
 # Setup Log
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(module)s - %(message)s')
