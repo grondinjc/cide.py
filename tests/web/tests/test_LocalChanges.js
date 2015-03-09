9be601d3d2055211d17b13ad0bc001021350362f
grondinjc <jean-christophe.grondin@usherbrooke.ca>
2015-03-09 18:41:55 -0400

    WIP - Adding tests
    This line cause errors when this._startAddedPos == 0 since it will
    forget the value and use the 'at' value ...
    this._startAddedPos = this._startAddedPos || at;

--- tests/web/tests/test_LocalChanges.js
+++ tests/web/tests/test_LocalChanges.js
@@ -1,3 +1,85 @@
-QUnit.test( "hello test", function( assert ) {
-  assert.ok( 1 == "1", "Passed!" );
+// hack waiting for eminent change by other team member
+CHANGE_RM_TYPE = -1;
+CHANGE_ADD_TYPE = 1; 
+
+QUnit.module( "test_localChanges", {
+  beforeEach: function() {
+    this.lc = new LocalChanges();
+  },
+  afterEach: function() {
+    // Nothing to be done
+  }
 });
+
+
+QUnit.test("testGet :: empty array", function(assert) {
+  var changes = this.lc.get();
+  assert.ok(changes.length == 0, "Default has no modification");
+});
+
+QUnit.test("testGet :: no char", function(assert) {
+  this.lc.addChange(0, "");
+  var changes = this.lc.get();
+  assert.ok(changes.length == 0, "There should be no elements");
+});
+
+QUnit.test("testGet :: invalid position", function(assert) {
+  this.lc.addChange(-1, "abc");
+  var changes = this.lc.get();
+  assert.ok(changes.length == 0, "There should be no elements");
+});
+
+QUnit.test("testGet :: no char ; invalid position", function(assert) {
+  this.lc.addChange(-1, "");
+  var changes = this.lc.get();
+  assert.ok(changes.length == 0, "There should be no elements");
+});
+
+
+
+QUnit.test("testAddChange :: one char", function(assert) {
+  this.lc.addChange(0, "1");
+  var changes = this.lc.get();
+  var expected = [createAddModif("1", 0)];
+  assert.ok(changes.length == 1, "There should only be one element");
+  assert.deepEqual(changes, expected, "Should be type add of '1' at 0");
+});
+
+QUnit.test("testAddChange :: contiguous char", function(assert) {
+  this.lc.addChange(0, "1");
+  this.lc.addChange(1, "2");
+  this.lc.addChange(2, "3");
+  var changes = this.lc.get();
+  var expected = [createAddModif("123", 0)];
+  assert.ok(changes.length == 1, "There should only be one element");
+  assert.deepEqual(changes, expected, "Should be type add of '123' at 0");
+});
+
+QUnit.test("testAddChange :: spaced char", function(assert) {
+  this.lc.addChange(0, "1");
+  this.lc.addChange(1, "2");
+  this.lc.addChange(2, "3");
+  var changes = this.lc.get();
+  var expected = [createAddModif("123", 0)];
+  assert.ok(changes.length == 1, "There should only be one element");
+  assert.deepEqual(changes, expected, "Should be type add of '123' at 0");
+});
+
+QUnit.test("testAddChange :: one block", function(assert) {
+  this.lc.addChange(0, "123");
+  var changes = this.lc.get();
+  var expected = [createAddModif("123", 0)];
+  assert.ok(changes.length == 1, "There should only be one element");
+  assert.deepEqual(changes, expected, "Should be type add of '123' at 0");
+});
+
+QUnit.test("testAddChange :: contiguous block", function(assert) {
+  this.lc.addChange(0, "123");
+  this.lc.addChange(3, "456");
+  this.lc.addChange(6, "789");
+  var changes = this.lc.get();
+  var expected = [createAddModif("123456789", 0)];
+  assert.ok(changes.length == 1, "There should only be one element");
+  assert.deepEqual(changes, expected, "Should be type add of '123456789' at 0");
+});
+
