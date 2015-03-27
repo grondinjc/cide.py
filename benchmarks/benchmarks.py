3cf941438b6c5070bb7d0ab120d48a6e95c07c83
Mariane Maynard <maym2104@dinf-0014-12b.DInf.fsci.usherbrooke.ca>
2015-03-27 10:08:25 -0400

    Adding some results

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -39,16 +39,15 @@ class Benchmarks(object):
     
     #debug()             
     self.core = None
-    self.callers = ["Arnold", "Benjamin", "Cecile", "Donald", "Eugene", "Francis", "Ginette", "Horace", "Ingrid", "Jean-Christophe", "Mariane", "Marc", "Vincent"]
-    self.files = ["/a", "/b/b", "/c/c/c", "/d/d/d/d", "/e/e/e/e/e", "/f/f/f/f/f/f", "/g/g/g/g/g/g/g", "/h/h/h/h/h/h/h/h", "/i/i/i/i/i/i/i/i/i", "/j/j/j/j/j/j/j/j/j/j"]
+    self.callers = []
+    self.files = []
     self.changes = []
     
-    for i in range(26):
-        self.callers.append(chr(ord('a')+i % 26))
+    for i in range(676):
+        self.callers.append(chr(ord('a')+i / 26) + chr(ord('a')+i % 26))
         filename = ""
         for j in range(random.randint(1,100)):
-            filename += '/'
-            filename += chr(ord('a')+i % 26)
+            filename += '/' + chr(ord('a')+i / 26) + chr(ord('a')+i % 26)
         self.files.append(filename)     
             
   def setUp(self):
@@ -111,10 +110,10 @@ class Benchmarks(object):
   def benchmarks_task_create_archive(self):
     print 'Test de _task_create_archive'
     caller = random.choice(self.callers)
-    self.myTimeIt(lambda: self.core._task_create_archive('.', caller, Queue()), 10)
+    self.myTimeIt(lambda: self.core._task_create_archive('.', caller, Queue()))
     print ' '
     
-  def myTimeIt(self, function, n=10000):
+  def myTimeIt(self, function, n=100):
     times = []
     for i in range(n):
       gc.disable()
@@ -134,9 +133,9 @@ class Benchmarks(object):
     print '10 best are : {0} {1} {2} {3} {4} {5} {6} {7} {8} {9}'.format(times[0],times[1],times[2],times[3],times[4],times[5],times[6],times[7],times[8],times[9])
     print '10 worst are : {0} {1} {2} {3} {4} {5} {6} {7} {8} {9}'.format(times[len(times)-1], times[len(times)-2], times[len(times)-3],times[len(times)-4],times[len(times)-5],times[len(times)-6],times[len(times)-7],times[len(times)-8],times[len(times)-9],times[len(times)-10])
     
-    print 'best case is : {0}'.format(min(times).microseconds)     
-    print 'worst case is : {0}'.format(max(times).microseconds)
-    print 'average is: {0}'.format((reduce(lambda x, y: x + y, times)/len(times)).microseconds)
+    print 'best case is : {0}'.format(min(times))     
+    print 'worst case is : {0}'.format(max(times))
+    print 'average is: {0}'.format((reduce(lambda x, y: x + y, times)/len(times)))
     
 benchmarks = Benchmarks()
 benchmarks.benchmarks_task_get_project_nodes()
