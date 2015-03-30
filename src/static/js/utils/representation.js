ecdc2db0d3cddd2f40be159834a8124794bcea1f
Vincent Philippon <Vincent.Philippon@USherbrooke.ca>
2015-03-30 19:23:25 -0400

    Client-side do not apply own modification

--- src/static/js/utils/representation.js
+++ src/static/js/utils/representation.js
@@ -26,9 +26,10 @@ function createExport(directory) { return { path: directory}; }
 function createChatMessage(msg) { return { message: msg}; }
 
 
-function ObjectAddChange(pos, content){
+function ObjectAddChange(pos, content, author){
   this._pos = pos;
   this._content = content;
+  this._author = author; // From server only
 }
 ObjectAddChange.prototype.size = function(){
   return this._content.length;
@@ -37,18 +38,25 @@ ObjectAddChange.prototype.applyOnText = function(text){
   return text.insert(this._content, this._pos);
 };
 ObjectAddChange.prototype.applyOnPos = function(pos){
-  return pos + (this._pos <= pos ? this.size() : 0);
+  if(!this._author){
+    return pos + (this._pos <= pos ? this.size() : 0);
+  } else {
+    return pos;
+  }
 };
 ObjectAddChange.prototype.updatePos = function(modObject){
-  this._pos = modObject.applyOnPos(this._pos);
+  if(!this._author){
+    this._pos = modObject.applyOnPos(this._pos);
+  }
 };
 ObjectAddChange.prototype.serialize = function(){
   return createAddModif(this._content, this._pos);
 };
 
-function ObjectRemoveChange(pos, count){
+function ObjectRemoveChange(pos, count, author){
   this._pos = pos;
   this._count = count;
+  this._author = author; // From server only
 }
 ObjectRemoveChange.prototype.size = function(){
   return this._count;
@@ -57,10 +65,16 @@ ObjectRemoveChange.prototype.applyOnText = function(text){
   return text.cutFrom(this._pos, this._count);
 };
 ObjectRemoveChange.prototype.applyOnPos = function(pos){
-  return pos - (this._pos <= pos ? this.size() : 0);
+  if(!this._author){
+    return pos - (this._pos <= pos ? this.size() : 0);
+  } else {
+    return pos;
+  }
 };
 ObjectRemoveChange.prototype.updatePos = function(modObject){
-  this._pos = modObject.applyOnPos(this._pos);
+  if(!this._author){
+    this._pos = modObject.applyOnPos(this._pos);
+  }
 };
 ObjectRemoveChange.prototype.serialize = function(){
   return createRemoveModif(this._count, this._pos);
@@ -69,8 +83,10 @@ ObjectRemoveChange.prototype.serialize = function(){
 
 function ObjectChangeFactory(changeObjJSON){
   return changeObjJSON.type == CHANGE_ADD_TYPE ?
-    new ObjectAddChange(changeObjJSON.pos, changeObjJSON.content):
-    new ObjectRemoveChange(changeObjJSON.pos, changeObjJSON.count);
+    new ObjectAddChange(changeObjJSON.pos, changeObjJSON.content,
+                        changeObjJSON.author):
+    new ObjectRemoveChange(changeObjJSON.pos, changeObjJSON.count,
+                           changeObjJSON.author);
 }
 
 function serializeObjectChangeList(changesList){
