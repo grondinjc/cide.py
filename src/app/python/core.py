925f92e28b85c1e979ab966c84eab56a18d7ecd4
Vincent Philippon <Vincent.Philippon@USherbrooke.ca>
2015-04-07 17:05:40 -0400

    WIP -  Pipe close & wait

--- src/app/python/core.py
+++ src/app/python/core.py
@@ -141,9 +141,10 @@ class Core(object):
     self._thread.stop()
     # Stop existing executions
     for execution in self._project_execs.itervalues():
-      execution.process.stdout.close()
       execution.process.stdin.close()
+      execution.process.stdout.close()
       execution.process.kill()  # brutal
+      execution.process.wait()
 
   def get_project_name(self):
     """
@@ -536,7 +537,9 @@ class Core(object):
     if caller in self._project_execs:
       user_execution = self._project_execs[caller]
       user_execution.process.stdin.close()
+      user_execution.process.stdout.close()
       user_execution.process.terminate()
+      user_execution.process.wait()
       del self._project_execs[caller]
     else:
       # Notify no process in progress
@@ -660,8 +663,8 @@ class Core(object):
         # Check if program exited
         if execution.process.poll() is not None:
           # Notify process end
-          execution.process.stdout.close()
           execution.process.stdin.close()
+          execution.process.stdout.close()
           exitcode = execution.process.wait()
           self._notify_event(lambda l: l.notify_program_ended(exitcode, caller))
           self._inner_task_remove_program_files(caller)
