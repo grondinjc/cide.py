da33cc19bc3f1dbf24bf30318a0e18ac0a4cb430
Jean-Christophe Grondin <jean-christophe.grondin@usherbrooke.ca>
2015-04-27 17:25:49 -0400

    Allow file to be run outside parent directory

--- tests/python/runner.py
+++ tests/python/runner.py
@@ -1,5 +1,6 @@
+from os.path import dirname
 import unittest
 
 if __name__ == "__main__":
-  testsuite = unittest.TestLoader().discover('.')
+  testsuite = unittest.TestLoader().discover(dirname(__file__))
   unittest.TextTestRunner(verbosity=1).run(testsuite)
